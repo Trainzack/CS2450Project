26d73ce8fbef250449581be9ff2d006d67d21292
Eli Zupke <eazupke@cpp.edu>
2019-05-07 13:59:07 -0700

    Fixed minor problems, reorganized control panel

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,12 @@ import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Bounds;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Separator;
 import javafx.scene.image.Image;
@@ -23,6 +25,11 @@ import javafx.stage.Stage;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.transform.Scale;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
@@ -53,6 +60,8 @@ class ZoomableScrollPane extends ScrollPane{
 
 public class Main extends Application{
 	
+	protected static final int CONTROL_WIDTH = 120;
+	
 	private Rectangle currentRectangle;
 	private ImageView currentImage;
 	
@@ -65,76 +74,26 @@ public class Main extends Application{
 		primaryStage.setTitle("PDF Cropper");
 
 		HBox canvas = new HBox();
+		canvas.setPadding(new Insets(5));
 
 		Pane imagePane = new Pane();
 		ZoomableScrollPane imageCanvas = new ZoomableScrollPane(imagePane);
 		VBox controlBox = new VBox();
-		controlBox.setMinWidth(100);
+		controlBox.setSpacing(10);
+		controlBox.setMinWidth(CONTROL_WIDTH + 15);
 		
 		canvas.getChildren().add(controlBox);
 		canvas.getChildren().add(imageCanvas);
 		Button loadFileButton = new Button("Load file...");
 		
-		
-		Button fullPicturePreset = new Button("Full picture");
-		controlBox.getChildren().add(fullPicturePreset);
-		
-		
-		fullPicturePreset.setOnAction(new EventHandler<ActionEvent>(){
 
-			@Override
-			public void handle(ActionEvent event) {
-				if (currentRectangle != null) {
-					imagePane.getChildren().remove(currentRectangle);
-				}
-				currentRectangle = new Rectangle(50,50,currentImage.getImage().getWidth(),currentImage.getImage().getHeight());
-				currentRectangle.setFill(new Color(0.4,0.4, 1,0.3));
-				currentRectangle.setStroke(new Color(0.4,0.4, 1,1));
-				currentRectangle.setId("RECT");
-				imagePane.getChildren().add(currentRectangle);
-				
-				
-			}
-			
-		});
-		
-		
-<<<<<<< HEAD
-		int[][] presets = {{50, 50, 200, 400}, {50, 50, 100, 400}, {50,50,300,500}, {50,50,500,700}};
-		String[] presetNames = {"4 x 3", "2 x 3", "5 x 7", "8.5 x 11"};
-=======
-		int[][] presets = {{150, 200, 700, 550}, {60, 60, 500, 390}};
-		
->>>>>>> c9651d3f6b840993ac933e692ca963ada56c0706
-		for (int i = 0; i < presets.length; i++) {
-			int[] preset = presets[i];
-			Button p = new Button(presetNames[i]);
-			controlBox.getChildren().add(p);
-			p.setOnAction(new EventHandler<ActionEvent>() {
-
-				@Override
-				public void handle(ActionEvent event) {
-					if (currentRectangle != null) {
-						imagePane.getChildren().remove(currentRectangle);
-					}
-					currentRectangle = new Rectangle(preset[0],preset[1],preset[2],preset[3]);
-					currentRectangle.setFill(new Color(0.4,0.4, 1,0.3));
-					currentRectangle.setStroke(new Color(0.4,0.4, 1,1));
-					currentRectangle.setId("RECT");
-					imagePane.getChildren().add(currentRectangle);
-					
-					//primaryStage.show();
-				}
-				
-			});
-		}
 		HBox zoomControls = new HBox();
-
-		controlBox.getChildren().add(new Separator());
 		
 		Button zoomInButton = new Button("+");
 		Button zoomOutButton = new Button("-");
+		zoomControls.setSpacing(10);
 		zoomControls.getChildren().add(zoomOutButton);
+		zoomControls.getChildren().add(new Label("Zoom"));
 		zoomControls.getChildren().add(zoomInButton);
 		
 		zoomInButton.setOnAction(new EventHandler<ActionEvent>() {
@@ -166,11 +125,9 @@ public class Main extends Application{
 		Scene scene = new Scene(canvas);
 		
 		//fileChooser.showOpenDialog(primaryStage);
-		
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		
-		
+	
+
+		loadFileButton.setMinWidth(CONTROL_WIDTH);
 		loadFileButton.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
@@ -197,6 +154,73 @@ public class Main extends Application{
 			}
 			
 		});
+		
+
+		VBox presetsPane = new VBox();
+		presetsPane.setBorder(new Border(new BorderStroke(Color.BLACK, 
+		            BorderStrokeStyle.SOLID, new CornerRadii(5), BorderWidths.DEFAULT)));
+		presetsPane.setSpacing(2);
+		presetsPane.setPadding(new Insets(5));
+		
+		controlBox.getChildren().add(new Separator());
+		controlBox.getChildren().add(presetsPane);
+		Label presetsLabel = new Label("Presets");
+		presetsPane.getChildren().add(presetsLabel);
+		
+		Button fullPicturePreset = new Button("Full picture");
+		presetsPane.getChildren().add(fullPicturePreset);
+		fullPicturePreset.setMaxWidth(CONTROL_WIDTH);
+		
+		fullPicturePreset.setOnAction(new EventHandler<ActionEvent>(){
+
+			@Override
+			public void handle(ActionEvent event) {
+				if (currentRectangle != null) {
+					imagePane.getChildren().remove(currentRectangle);
+				}
+				if (currentImage == null) {
+					return;
+				}
+				currentRectangle = new Rectangle(50,50,currentImage.getImage().getWidth(),currentImage.getImage().getHeight());
+				currentRectangle.setFill(new Color(0.4,0.4, 1,0.3));
+				currentRectangle.setStroke(new Color(0.4,0.4, 1,1));
+				currentRectangle.setId("RECT");
+				imagePane.getChildren().add(currentRectangle);
+				
+				
+			}
+			
+		});
+		
+		int[][] presets = {{150, 200, 700, 550}, {60, 60, 500, 390}, {50, 50, 200, 400}, {50, 50, 100, 400}, {50,50,300,500}, {50,50,500,700}};
+		String[] presetNames = {"Custom 1", "Custom 2", "4 x 3", "2 x 3", "5 x 7", "8.5 x 11"};
+
+		for (int i = 0; i < presets.length; i++) {
+			int[] preset = presets[i];
+			Button p = new Button(presetNames[i]);
+			p.setMinWidth(CONTROL_WIDTH);
+			presetsPane.getChildren().add(p);
+			p.setOnAction(new EventHandler<ActionEvent>() {
+
+				@Override
+				public void handle(ActionEvent event) {
+					if (currentRectangle != null) {
+						imagePane.getChildren().remove(currentRectangle);
+					}
+					currentRectangle = new Rectangle(preset[0],preset[1],preset[2],preset[3]);
+					currentRectangle.setFill(new Color(0.4,0.4, 1,0.3));
+					currentRectangle.setStroke(new Color(0.4,0.4, 1,1));
+					currentRectangle.setId("RECT");
+					imagePane.getChildren().add(currentRectangle);
+					
+					//primaryStage.show();
+				}
+				
+			});
+		}
+		
+		primaryStage.setScene(scene);
+		primaryStage.show();
 		
 	}
 	
